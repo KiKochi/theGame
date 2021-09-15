f767d8899fcf295c4fde6184ce658a1f20a07647
KiKoChi <akrm.s.q@gmail.com>
2021-09-15 02:10:13 +0200

    add: DOM classes selecters

--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -1,17 +1,29 @@
 const monsterHealthBar = document.getElementById('monster-health');
 const playerHealthBar = document.getElementById('player-health');
+const playerManaBar = document.getElementById('player-mana');
 const bonusLifeEl = document.getElementById('bonus-life');
 
 const attackBtn = document.getElementById('attack-btn');
-const skillAttackBtn = document.getElementById('skill-attack-btn');
-const healBtn = document.getElementById('heal-btn');
+const chargeAttackBtn = document.getElementById('charge-attack-btn');
+const skillBtn = document.getElementById('skill-btn');
 const logBtn = document.getElementById('log-btn');
 
-function adjustHealthBars(monsterMaxLife, playerMaxLife) {
+const healtSec = document.getElementById('health-levels');
+const controlSec = document.getElementById('controls');
+const inputSec = document.getElementById('user-inputs');
+
+const username = document.getElementById('username');
+const warriorBtn = document.getElementById('warrior-btn');
+const mageBtn = document.getElementById('mage-btn');
+const assassinBtn = document.getElementById('assassin-btn');
+
+function adjustHealthBars(monsterMaxLife, playerMaxLife, playerMaxMana) {
   monsterHealthBar.max = monsterMaxLife;
   monsterHealthBar.value = monsterMaxLife;
   playerHealthBar.max = playerMaxLife;
   playerHealthBar.value = playerMaxLife;
+  playerManaBar.max = playerMaxMana;
+  playerManaBar.value = playerMaxMana;
 }
 
 function increasePlayerHealth(healValue) {
